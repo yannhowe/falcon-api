b02bca5ed290b96c5597995f37804a074d3fd0a0
Yann Howe <yannhowe.kwan@crowdstrike.com>
2024-01-31 14:12:49 +0800

    Add more fields into CSV

--- cspm/get-policy-data/get-policy-list.py
+++ cspm/get-policy-data/get-policy-list.py
@@ -16,14 +16,14 @@ with open('policy_details_all.json', 'w') as policy_details_all_file:
 
 # Now to make a CSV with just some parts of the data
 # Create CSV header
-column_headers = ['name', 'cloud_provider', 'cloud_service_friendly', 'cloud_service', 'cloud_service_subtype']
+column_headers = ['name', 'cloud_provider', 'cloud_service_friendly', 'cloud_service', 'cloud_service_subtype', 'policy_type', 'severity', 'default_severity', 'is_remediable' ]
 with open('policy_names_and_service.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames = column_headers)
     writer.writeheader()
 
 # Find matches and output value in CSV
 for policy_details in policy_details_all["body"]["resources"]:
-    policy_details_and_benchmark_ids = {key: value for key, value in policy_details.items() if "name" in key or "cloud_service" in key or "cloud_provider" in key}
+    policy_details_and_benchmark_ids = {key: value for key, value in policy_details.items() if "name" in key or "cloud_provider" in key or "cloud_service" in key or "policy_type" in key or "severity" in key or "default_severity" in key or "is_remediable" in key}
     print(policy_details_and_benchmark_ids)
     with open('policy_names_and_service.csv', 'a') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames = column_headers)
